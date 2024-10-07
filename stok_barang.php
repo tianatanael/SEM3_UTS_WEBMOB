df1e363ae302b9d785053c38d21f87c930b8c9a3
Tiana <tianatanael12@gmail.com>
2024-10-07 14:26:15 +0700

    Mengganti title jmlhbrg di index, mengganti route css di barang supaya terhubung

--- stok_barang.php
+++ stok_barang.php
@@ -41,7 +41,7 @@ if (isset($_SESSION['message'])) {
 }
 ?>
 
-<link rel="stylesheet" href="/Lat_HRD/CSS/barang.css">
+<link rel="stylesheet" href="/JAYA_GUDANG/CSS/barang.css">
 <!-- Bootstrap 5 source -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
